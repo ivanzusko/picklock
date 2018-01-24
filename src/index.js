1cfb8507f89b4e7e836d585e431899a4c10a3863
ivanzusko <ivan.zusko@gmail.com>
2018-01-24 10:50:16 +0100

    Add 'log' helper

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export * from './addProperty';
 export * from './cloneObject';
 export * from './deleteProperty';
+export * from './log';
 export * from './typeOf';
 export * from './updateProperty';
---
+++ src/log.js
@@ -0,0 +1,6 @@
+export default function log(...args) {
+  const [label, ...restArgs] = [...args];
+
+  return console.log(`%c${label}:`, 'background: #222; color: #bada55; padding: 3px 5px;', ...restArgs);
+};
+
