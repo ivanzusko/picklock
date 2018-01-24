27bb9ed56376f54121bec0b6cef3b77bde9827b7
ivanzusko <ivan.zusko@gmail.com>
2018-01-24 10:59:14 +0100

    Remove default export

--- src/log.js
+++ src/log.js
@@ -1,4 +1,4 @@
-export default function log(...args) {
+export function log(...args) {
   const [label, ...restArgs] = [...args];
 
   return console.log(`%c${label}:`, 'background: #222; color: #bada55; padding: 3px 5px;', ...restArgs);
